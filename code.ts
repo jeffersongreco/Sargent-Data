90f58577b2b70f50df727bf1681232633aba47a6
Greco <greco.grco@gmail.com>
2024-10-22 15:41:21 -0300

    lint and format

--- code.ts
+++ code.ts
@@ -4,4 +4,4 @@
 // full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__, { themeColors: true, width: 1133, height: 320, title: "Sargent" })
+figma.showUI(__html__, { themeColors: true, width: 1000, height: 320, title: "Sargent" })
