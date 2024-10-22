fac639d19ba61037a4d6891df680930338c6b72a
Greco <greco.grco@gmail.com>
2024-10-22 12:40:50 -0300

    first commit

---
+++ code.ts
@@ -0,0 +1,7 @@
+// This file holds the main code for plugins. Code in this file has access to
+// the *figma document* via the figma global object.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
+
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__, { themeColors: true, width: 1133, height: 320, title: "Sargent" })
